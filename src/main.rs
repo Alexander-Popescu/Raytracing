3b1a3304d92327d62cbb4bf9cf00baf31032caaa
Alexander Popescu <alexanderpopescu5381@gmail.com>
2023-02-20 16:55:12 -0500

    fixed ovular shape

--- src/main.rs
+++ src/main.rs
@@ -41,9 +41,9 @@ fn main() {
     let focal_length = 1.0;
 
     let origin = Vec3::new(0.0,0.0,0.0);
-    let horizontal = Vec3::new(viewport_width,0.0,0.0);
-    let vertical = Vec3::new(0.0,viewport_height,0.0);
-    let lower_left_corner = origin - horizontal / 2.0 - vertical / 2.0 - Vec3::new(0.0,0.0, focal_length);
+    let horizontal = Vec3::new(4.0,0.0,0.0);
+    let vertical = Vec3::new(0.0,2.0,0.0);
+    let lower_left_corner = Vec3::new(-2.0,-1.0,-1.0);
 
 
     //camera do
